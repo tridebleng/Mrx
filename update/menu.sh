87d2360b0d02f71b88c16575ae16ec3656f5d296
tridebleng <110109155+tridebleng@users.noreply.github.com>
2022-08-08 22:32:29 +0700

    Up
    Date

--- update/menu.sh
+++ update/menu.sh
@@ -41,18 +41,20 @@ echo -e "\E[41;1;39m                     ⇱ MENU  PANEL VPS ⇲                
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "
 [${green}01${NC}]${color1} •${color3}$bd PANEL SSH & OVPN (${color2}menu-ssh${color3})
-[${green}02${NC}]${color1} •${color3}$bd PANEL Wireguard (${color2}menu-wireguard${color3})
+[${green}02${NC}]${color1} •${color3}$bd PANEL V2ray Vmess & Vless (${color2}menu-wireguard${color3})
 [${green}03${NC}]${color1} •${color3}$bd PANEL L2tp & PPTP Acpunt (${color2}menu-l2tp-pptp${color3})
-[${green}04${NC}]${color1} •${color3}$bd PANEL V2ray Vmess & Vless (${color2}menu-v2ray${color3})
+[${green}04${NC}]${color1} •${color3}$bd PANEL Wireguard (${color2}menu-v2ray${color3})
 [${green}05${NC}]${color1} •${color3}$bd PANEL SSR & SS Acount (${color2}menu-ssr-ss${color3})
 [${green}06${NC}]${color1} •${color3}$bd PANEL Trojan (${color2}menu-trojan${color3})
 [${green}07${NC}]${color1} •${color3}$bd GANTI Port All Acount (${color2}menu-port${color3})
-[${green}08${NC}]${color1} •${color3}$bd CEK status layanan (${color2}running${color3})
+[${green}08${NC}]${color1} •${color3}$bd CEK Status Layanan (${color2}running${color3})
 [${green}09${NC}]${color1} •${color3}$bd MENU Tools (${color2}tools${color3})
 [${green}10${NC}]${color1} •${color3}$bd WEB menu (${color2}webmin${color3})
-[${green}11${NC}]${color1} •${color3}$bd INFO script auto install (${color2}info${color3})
-[${green}12${NC}]${color1} •${color3}$bd Reboot vps (${color2}reboot${color3})
-[${green}13${NC}]${color1} •${color3}$bd Restart vps (${color2}restart${color3})
+[${green}11${NC}]${color1} •${color3}$bd INFO Script ARTA MAULANA (${color2}info${color3})
+[${green}12${NC}]${color1} •${color3}$bd Cek Bandwitch (${color2}vnstat${color3})
+[${green}13${NC}]${color1} •${color3}$bd Cek Bandwitch Harian (${color2}vnstat -d${color3})
+[${green}14${NC}]${color1} •${color3}$bd Reboot VPS (${color2}reboot${color3})
+[${green}15${NC}]${color1} •${color3}$bd Restart VPS (${color2}restart${color3})
 
 [${green}00${NC}]${color1} •${color3}$bd Back to exit Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
 echo ""
@@ -67,13 +69,13 @@ case $menu in
 ssh
 ;;
 2)
-wgr
+v2raay
 ;;
 3)
 l2tp
 ;;
 4)
-v2raay
+wgr
 ;;
 5)
 ssssr
@@ -97,9 +99,15 @@ wbmn
 info
 ;;
 12)
-reboot
+vnstat
 ;;
 13)
+vnstat -d
+;;
+14)
+reboot
+;;
+15)
 restart
 ;;
 0 | 00)
