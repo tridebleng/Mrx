2eef05630d0f85dca6db42d43d1bbd8d00d7b6de
tridebleng <110109155+tridebleng@users.noreply.github.com>
2022-08-22 15:54:06 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -38,30 +38,28 @@ echo -e " \E[41;1;39m                     ⇱ INFORMASI VPS ⇲                 
 echo -e "\033[0;34m╘═════════════════════════════════════════════════════════════╛\033[0m${NC}"
 echo -e " ❇️$bd ISP Name          ${color1} •${color3}$bd $ISP"
 echo -e " ❇️$bd City              ${color1} •${color3}$bd $CITY"
-echo -e " ❇️$bd Total RAM         ${color1} •${color3}$bd $tram MB"
+echo -e " ❇️$bd Total RAM         ${color1} •${yl}$bd $tram MB${NC}"
 echo -e " ❇️$bd IP VPS            ${color1} •${color3}$bd $MYIP"
-echo -e " ❇️$bd DOMAIN VPS        ${color1} •${color3}$bd $DOMAIN"
-echo -e " ❇️$bd Waktu Aktif       ${color1} •${color3}$bd $up"
+echo -e " ❇️$bd DOMAIN VPS        ${color1} •${CYAN}$bd $DOMAIN${NC}"
+echo -e " ❇️$bd Waktu Aktif       ${color1} •${or}$bd $up${NC}"
 echo -e " ❇️$bd Client Name       ${color1} •${color3}$bd \033[1;36mARTA MAULANA VPN${NC}"
 echo -e " ❇️$bd Expiry script     ${color1} •${color3}$bd \033[1;32mLIFETIME${NC}"
 echo -e "\033[0;34m╒═════════════════════════════════════════════════════════════╕\033[0m${NC}"
 echo -e " \E[41;1;39m                     ⇱ MENU PANEL VPS ⇲                      \E[0m"
 echo -e "\033[0;34m╘═════════════════════════════════════════════════════════════╛\033[0m${NC}"
-echo -e "
-[${GREEN}01${NC}]${color1} •${color3}$bd PANEL SSH & OpenVPN     [${GREEN}09${NC}]${color1} •${color3}$bd MENU SETTING [\033[1;32mPro${NC}]
+echo -e "[${GREEN}01${NC}]${color1} •${color3}$bd PANEL SSH & OpenVPN     [${GREEN}09${NC}]${color1} •${color3}$bd MENU SETTING [\033[1;32mPro${NC}]
 [${GREEN}02${NC}]${color1} •${color3}$bd PANEL Vmess & Vless     [${GREEN}10${NC}]${color1} •${color3}$bd WEB MENU
 [${GREEN}03${NC}]${color1} •${color3}$bd PANEL L2tp & PPTP       [${GREEN}11${NC}]${color1} •${color3}$bd INFO Script \033[1;36mARTA MAULANA${NC}
 [${GREEN}04${NC}]${color1} •${color3}$bd PANEL Wireguard         [${GREEN}12${NC}]${color1} •${color3}$bd Cek Bandwitch
-[${GREEN}05${NC}]${color1} •${color3}$bd PANEL SSR & Shdsok      [${GREEN}13${NC}]${color1} •${color3}$bd Cek Bandwitch Harian
+[${GREEN}05${NC}]${color1} •${color3}$bd PANEL SSR & Shdsok      [${GREEN}13${NC}]${color1} •${color3}$bd Cek Bandwitch \033[1;33mHarian${NC}
 [${GREEN}06${NC}]${color1} •${color3}$bd PANEL Tr GPR & GO       [${GREEN}14${NC}]${color1} •${color3}$bd Reboot VPS
 [${GREEN}07${NC}]${color1} •${color3}$bd GANTI Port All Acount   [${GREEN}15${NC}]${color1} •${color3}$bd Restart VPS
 [${GREEN}08${NC}]${color1} •${color3}$bd CEK Status \033[1;32mRUNNING${NC}      [${GREEN}00${NC}]${color1} •${color3}$bd \033[1;31mCEK AREK AKTIF${NC} \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
-echo ""
 echo -e "\033[0;34m╒═════════════════════════════════════════════════════════════╕\033[0m${NC}"
-echo -e " \E[41;1;39m                     ⇱ ARTA VPS PROJECT ⇲                    \E[0m"
+echo -e " \E[41;1;39m                   ⇱ BIGETRON RED ALIENS ⇲                    \E[0m"
 echo -e "\033[0;34m╘═════════════════════════════════════════════════════════════╛\033[0m${NC}"
 echo -e  ""
- read -p "  Select menu :  " menu
+ read -p "  ${yl}Select menu${NC} :  " menu
 echo -e   ""
 case $menu in
 1)
