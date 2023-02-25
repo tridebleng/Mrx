102db3385fc54c945033bf239bd297356ac0505a
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-02-26 01:08:10 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -32,14 +32,13 @@ fi
 echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
 sleep 2
 echo -e "[ ${green}INFO$NC ] Getting acme for cert"
-wget autosc.me/acme.sh >/dev/null 2>&1
-bash acme.sh --install >/dev/null 2>&1
-bash acme.sh --register-account -m admin@fsidvpn.live
-wget https://get.acme.sh >/dev/null 2>&1 | sh -s email=admin@fsidvpn.live
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
-/root/.acme.sh/acme.sh --issue -d $domain --standalone --force --keylength ec-256
-/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+mkdir -p /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 echo -e "[ ${green}INFO${NC} ] Renew cert done... "
 sleep 5
 rm acme.sh >/dev/null 2>&1
