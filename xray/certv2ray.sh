bce18fd1a23397676032847b6e1b4c2f88279df5
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-02-26 01:30:01 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # ==========================================
 # Color
-RED='\033[0;31m'
+red='\033[0;31m'
 NC='\033[0m'
-#GREEN='\033[0;32m'
-#ORANGE='\033[0;33m'
+green='\033[0;32m'
+ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
-#CYAN='\033[0;36m'
+CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
-off='\x1b[m'
 # ==========================================
 # Getting
-
 clear
-echo start
-sleep 0.5
-source /var/lib/fsidvpn/ipvps.conf
-domain=$(cat /etc/xray/domain)
+clear
+echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "${green}               • RENEW DOMAIN SSL •               $NC"
+echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e ""
 echo -e "[ ${green}INFO${NC} ] Start " 
-sleep 1
+sleep 0.5
+systemctl stop nginx
+domain=$(cat /root/domain)
 Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
 if [[ ! -z "$Cek" ]]; then
 sleep 1
@@ -31,16 +32,17 @@ sleep 1
 fi
 echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
 sleep 2
-echo -e "[ ${green}INFO$NC ] Getting acme for cert"
-mkdir -p /root/.acme.sh
-curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
-chmod +x /root/.acme.sh/acme.sh
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+/root/.acme.sh/acme.sh --issue -d $domain --force --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
-echo -e "[ ${green}INFO${NC} ] Renew cert done... "
-sleep 5
-rm acme.sh >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] Renew cert done... "
-restart
+echo -e "[ ${green}INFO${NC} ] Renew cert done... " 
+sleep 2
+echo -e "[ ${green}INFO${NC} ] Starting service $Cek " 
+sleep 2
+echo $domain > /etc/xray/domain
+systemctl restart $Cek
+systemctl restart nginx
+echo -e "[ ${green}INFO${NC} ] All finished... " 
+sleep 0.5
+echo ""
+echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
